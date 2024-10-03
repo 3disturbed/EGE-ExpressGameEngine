9d4aa7a03899f172ec1f8fa90970da7cfd8904fe
Darklington <codemonkey@3disturbedmonkeys.co.uk>
2024-10-03 21:32:09 +0100

    add database.js

---
+++ database.js
@@ -0,0 +1,21 @@
+
+// database.js
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+
+function connectDB() {
+  // Replace with your MongoDB connection string
+  const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/EGE-ExpressGameEngine';
+
+  mongoose
+    .connect(mongoURI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+}
+
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.MONGO_URI ||  'mongodb://localhost:27017/EGE-ExpressGameEngine',
+  collectionName: 'sessions',
+});
+
+module.exports = { connectDB, sessionStore };
